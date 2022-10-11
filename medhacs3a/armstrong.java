abf045136296eecef34fb509fe1b53f233550dca
Medha <98379619+m3kvt@users.noreply.github.com>
2022-10-12 00:08:39 +0530

    Update armstrong.java

--- medhacs3a/armstrong.java
+++ medhacs3a/armstrong.java
@@ -1,26 +1,29 @@
 import java.util.*;
 import java.lang.Math;
 class armstrong{
-  public static void main(String a[]){
-    int digit,digits=0,sum=0;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter a number:");
-    int num=sc.nextInt();
-    while(num!=0){
-      digit=num%10;
-      digits+=1;
-      num=num/10;
-    }
-    while(num!=0){
-      digit=num%10;
-      sum+=pow(digit,digits);
-      num=num/10;
-      
-    }
-    if (sum==num){
+public static void main(String a[]){
+int temp,n, digits=0, last=0, sum=0;
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter the number to be checked: ");   
+n=sc.nextInt();  
+temp=n;    
+while(temp>0)    
+{   
+temp = temp/10;   
+digits++;   
+}   
+temp = n;   
+while(temp>0)   
+{   
+last = temp % 10;   
+sum +=  (Math.pow(last, digits));   
+temp = temp/10;   
+}  
+    if (sum==n){
       System.out.println("It is an armstrong number");
     }
     else
       System.out.println("Not an armstrong number");
+    
   }
 }
